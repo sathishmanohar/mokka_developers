f5b62e82c841c0e618fd1f301e44a567cc73e7b8
Sathish Manohar <design.sathish@gmail.com>
2013-04-15 13:06:05 +0530

    basic topic CRUD

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,50 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(params[:topic])
+    if @topic.save
+      flash[:success] = "Topic created"
+      redirect_to topic_url(@topic)
+    else
+      flash[:error] = "Error creating topic"
+      render "new"
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update_attributes(params[:topic])
+        flash[:success] = "Topic updated"
+        redirect_to topic_url(@topic)
+      else
+        flash[:error] = "Error updating topic"
+        render "edit" 
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    if @topic.destroy
+      flash[:success] = "Topic Deleted"
+      redirect_to topics_url
+    else
+      flash[:error] = "Error Deleting topic"
+      render "edit"
+    end
+  end
+end
---
+++ app/models/topic.rb
@@ -0,0 +1,3 @@
+class Topic < ActiveRecord::Base
+  attr_accessible :body, :title
+end
